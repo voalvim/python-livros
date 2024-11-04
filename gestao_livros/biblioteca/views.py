6d4a76be2842eea0055c66646e803b8c33f8e13c
Vitor Ornelas <ornelas.dev@gmail.com>
2024-11-04 19:46:25 -0300

    Create ViewSets inside views.py

--- gestao_livros/biblioteca/views.py
+++ gestao_livros/biblioteca/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from .models import Autor, Livro
+from .serializers import AutorSerializer, LivroSerializer
+
+class AutorViewSet(viewsets.ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+class LivroViewSet(viewsets.ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
