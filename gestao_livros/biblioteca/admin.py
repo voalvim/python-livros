46c23e319f3392f0cc2bb015e24511053c592038
Vitor Ornelas <ornelas.dev@gmail.com>
2024-11-04 19:57:38 -0300

    Enabling models inside admin.py

--- gestao_livros/biblioteca/admin.py
+++ gestao_livros/biblioteca/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Autor, Livro
 
-# Register your models here.
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome')
+    search_fields = ['nome']
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'autor', 'data_publicacao', 'numero_paginas')
+    search_fields = ['titulo']
+    list_filter = ['autor', 'data_publicacao']
