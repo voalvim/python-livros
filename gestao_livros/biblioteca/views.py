a87395f1ec2d2dabb227c65fc336e1c8d71358a7
Vitor Ornelas <ornelas.dev@gmail.com>
2024-11-04 19:55:14 -0300

    Adding custom methods + bugfix

--- gestao_livros/biblioteca/views.py
+++ gestao_livros/biblioteca/views.py
@@ -2,14 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status, filters
+from rest_framework.response import Response
 from .models import Autor, Livro
 from .serializers import AutorSerializer, LivroSerializer
+from rest_framework.decorators import action
 
 class AutorViewSet(viewsets.ModelViewSet):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nome']
+
+    def create(self, request, *args, **kwargs):
+        if Autor.objects.filter(nome=request.data.get('nome')).exists():
+            return Response({"detail": "Autor já existe."}, status=status.HTTP_400_BAD_REQUEST)
+        return super().create(request, *args, **kwargs)
+
 
 class LivroViewSet(viewsets.ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['titulo', 'autor__nome']
