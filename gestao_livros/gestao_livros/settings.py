2557cf89e19602d4292c4d8f38e67a4fd0ec4ce3
Vitor Ornelas <ornelas.dev@gmail.com>
2024-11-04 20:16:43 -0300

    Install JWT and change Settings.py

--- gestao_livros/gestao_livros/settings.py
+++ gestao_livros/gestao_livros/settings.py
@@ -51,6 +51,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
 ROOT_URLCONF = 'gestao_livros.urls'
 
 TEMPLATES = [
