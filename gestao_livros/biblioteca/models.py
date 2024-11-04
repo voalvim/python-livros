a9a94861f256d6af947e4b927a34e10cacb43af6
Vitor Ornelas <ornelas.dev@gmail.com>
2024-11-04 19:59:34 -0300

    Creating models the right way

--- gestao_livros/biblioteca/models.py
+++ gestao_livros/biblioteca/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+def __str__(self):
+    return self.nome
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.ForeignKey(Autor, related_name='livros', on_delete=models.CASCADE)
+    data_publicacao = models.DateField()
+    numero_paginas = models.IntegerField()
+
+def __str__(self):
+    return self.titulo
