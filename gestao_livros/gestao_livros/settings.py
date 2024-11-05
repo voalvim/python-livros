7b25d8bfa744e7eaaf4d9689a23447e0386f4a78
Vitor Ornelas <ornelas.dev@gmail.com>
2024-11-04 23:21:39 -0300

    Exercicio 3

--- gestao_livros/gestao_livros/settings.py
+++ gestao_livros/gestao_livros/settings.py
@@ -58,6 +58,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,  #
 }
 
 ROOT_URLCONF = 'gestao_livros.urls'
