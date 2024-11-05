689671fa00dd66aeb0929069983814b4c68c9641
Vitor Ornelas <ornelas.dev@gmail.com>
2024-11-04 23:28:00 -0300

    Exercicio 4

--- gestao_livros/biblioteca/models.py
+++ gestao_livros/biblioteca/models.py
@@ -1,15 +1,25 @@
-from django.db import models
+from django.core.exceptions import ValidationError
 from django.db import models
 
 class Autor(models.Model):
     nome = models.CharField(max_length=100)
-def __str__(self):
-    return self.nome
+
+    def __str__(self):
+        return self.nome
+
 class Livro(models.Model):
     titulo = models.CharField(max_length=200)
     autor = models.ForeignKey(Autor, related_name='livros', on_delete=models.CASCADE)
     data_publicacao = models.DateField()
     numero_paginas = models.IntegerField()
 
-def __str__(self):
-    return self.titulo
+    def clean(self):
+        if not self.autor:
+            raise ValidationError('O livro deve ter um autor.')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.titulo
