e742ca3c40b1754d2e5f28322cea106c58530696
Vitor Ornelas <ornelas.dev@gmail.com>
2024-11-04 23:16:13 -0300

    Exercicio 2

--- gestao_livros/biblioteca/admin.py
+++ gestao_livros/biblioteca/admin.py
@@ -11,3 +11,9 @@ class LivroAdmin(admin.ModelAdmin):
     list_display = ('id', 'titulo', 'autor', 'data_publicacao', 'numero_paginas')
     search_fields = ['titulo']
     list_filter = ['autor', 'data_publicacao']
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
